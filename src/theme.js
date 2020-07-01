2947d384d38b2cbbbdb9c893dab1cc8c57129455
Oleg Ugrinchuk <ugrinchuko@gmail.com>
2020-07-01 07:08:21 +0300

    added theme

--- src/theme.js
+++ src/theme.js
@@ -2,3 +2,32 @@ const Theme = {
   LIGHT: 'light-theme',
   DARK: 'dark-theme',
 };
+
+const bodyRef = document.querySelector('body');
+const switcherRef = document.querySelector('.js-switch-input');
+
+switcherRef.addEventListener('change', savedTheme);
+
+function defaultTheme() {
+  if (localStorage.getItem('Theme') === Theme.DARK) {
+    bodyRef.classList.add(Theme.DARK);
+    switcherRef.checked = true;
+    bodyRef.classList.remove(Theme.LIGHT);
+  } else {
+    localStorage.setItem('Theme', 'light-theme');
+    bodyRef.classList.add(Theme.LIGHT);
+  }
+};
+defaultTheme();
+
+function savedTheme(event) {
+  if (event.target.checked) {
+    bodyRef.classList.add(Theme.DARK);
+    bodyRef.classList.remove(Theme.LIGHT);
+    localStorage.setItem('Theme', 'dark-theme');
+  } else if (!event.target.checked) {
+    bodyRef.classList.add(Theme.LIGHT);
+    bodyRef.classList.remove(Theme.DARK);
+    localStorage.setItem('Theme', 'light-theme');
+  }
+}
